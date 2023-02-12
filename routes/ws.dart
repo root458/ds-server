7aa20fec9e81e4c9c3605bc0662f7ac30b20adb8
Hope Kagame <kagame458@protonmail.com>
2023-02-12 15:11:32 +0300

    Remove logger

--- routes/ws.dart
+++ routes/ws.dart
@@ -1,21 +1,20 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
-import 'package:logger/logger.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   final handler = webSocketHandler(
     (channel, protocol) {
       // A new client has connected to our server.
-      Logger().i('connected');
+      print('connected');
 
       // Send a message to the client.
       channel.sink.add('hello from the server');
 
       // Listen for messages from the client.
       channel.stream.listen(
-        Logger().i,
+        print,
         // The client has disconnected.
-        onDone: () => Logger().i('disconnected'),
+        onDone: () => print('disconnected'),
       );
     },
   );
