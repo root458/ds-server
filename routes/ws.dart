2cd4a8ca2598e94747c86ce49ea6a782c9fe0dba
Hope Kagame <kagame458@protonmail.com>
2023-02-12 14:45:34 +0300

    Respond to ws connections

--- routes/ws.dart
+++ routes/ws.dart
@@ -1,5 +1,24 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+import 'package:logger/logger.dart';
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'You have requested /ws');
+Future<Response> onRequest(RequestContext context) async {
+  final handler = webSocketHandler(
+    (channel, protocol) {
+      // A new client has connected to our server.
+      Logger().i('connected');
+
+      // Send a message to the client.
+      channel.sink.add('hello from the server');
+
+      // Listen for messages from the client.
+      channel.stream.listen(
+        Logger().i,
+        // The client has disconnected.
+        onDone: () => Logger().i('disconnected'),
+      );
+    },
+  );
+
+  return handler(context);
 }
