4919eaed1681305bd3f18a3324e9fbb91079e0d2
Hope Kagame <kagame458@protonmail.com>
2023-02-18 16:05:43 +0300

    Register connmanager provider

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,4 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:distributed_server/connmanager/middleware/connmanager_provider.dart';
 import 'package:distributed_server/counter/counter.dart';
 
-Handler middleware(Handler handler) => handler.use(counterProvider);
+Handler middleware(Handler handler) => handler
+  ..use(counterProvider)
+  ..use(connManagerProvider);
