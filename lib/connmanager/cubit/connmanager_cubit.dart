5f9d53d781f5974dcf96c6fd5f3b77f37dd9daa4
Hope Kagame <kagame458@protonmail.com>
2023-02-18 16:51:26 +0300

    Enable addition and removal of connections

--- lib/connmanager/cubit/connmanager_cubit.dart
+++ lib/connmanager/cubit/connmanager_cubit.dart
@@ -3,11 +3,36 @@ import 'package:broadcast_bloc/broadcast_bloc.dart';
 /// ConnManager Cubit
 class ConnManagerCubit extends BroadcastCubit<int> {
   /// Create an instance with an initial state of 0.
-  ConnManagerCubit() : super(0);
+  ConnManagerCubit(int maxConnections) : super(0) {
+    connections = <int, String>{};
+    maxConnections = maxConnections;
+  }
 
-  /// Increment the current state.
-  void increment() => emit(state + 1);
+  /// Total number of connections possible
+  late int maxConnections;
 
-  /// Decrement the current state.
-  void decrement() => emit(state - 1);
+  /// Storing the current connections
+  late Map<int, String> connections;
+
+  /// Add connection
+  void addConnectedClient(String address) {
+    if (connections.containsValue(address)) {
+      return;
+    } else {
+      for (var index = 0; index < maxConnections; index++) {
+        if (connections.containsKey(index)) {
+          continue;
+        } else {
+          connections[index] = address;
+        }
+      }
+    }
+  }
+
+  /// Remove connection
+  void removeConnectedClient(String address) =>
+      connections.removeWhere((key, value) => value == address);
+
+  /// Upgrade connection
+  void upgradeConnection(String address) {}
 }
