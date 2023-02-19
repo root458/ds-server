9f4c3169682ea4b9bcd1d761604f52f6d81b763e
Hope Kagame <kagame458@protonmail.com>
2023-02-19 17:27:40 +0300

    Listen and send color changes

--- routes/ws.dart
+++ routes/ws.dart
@@ -1,7 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+import 'package:distributed_server/colorchange/color_change.dart';
 import 'package:distributed_server/connmanager/connmanager.dart';
-import 'package:distributed_server/counter/counter.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   // Initialize conn manager
@@ -18,32 +18,19 @@ Future<Response> onRequest(RequestContext context) async {
       // A new client has connected to our server.
       // Subscribe the new client to receive notifications
       // whenever the cubit state changes.
-      final counterCubit = context.read<CounterCubit>()..subscribe(channel);
+      final colorChangeCubit = context.read<ColorChangeCubit>()
+        ..subscribe(channel);
 
-      // Send the current count to the new client.
-      channel.sink.add('${counterCubit.state}');
+      // Send the current color to the new client.
+      channel.sink.add(colorChangeCubit.state);
 
       // Listen for messages from the client.
       channel.stream.listen(
-        (event) {
-          switch (event) {
-            // Handle an increment message.
-            case '__increment__':
-              counterCubit.increment();
-              break;
-            // Handle a decrement message.
-            case '__decrement__':
-              counterCubit.decrement();
-              break;
-            // Ignore any other messages.
-            default:
-              break;
-          }
-        },
+        (newColorReq) => colorChangeCubit.changeColor(newColorReq as String),
         // The client has disconnected.
         // Unsubscribe the channel.
         onDone: () {
-          counterCubit.unsubscribe(channel);
+          colorChangeCubit.unsubscribe(channel);
           connManagerCubit.removeConnectedClient(address.address);
         },
       );
