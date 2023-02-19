15cc193332ab712cc793bb645ed6b31976e01c1f
Hope Kagame <kagame458@protonmail.com>
2023-02-19 17:25:20 +0300

    Remove counter provider

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,11 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:distributed_server/colorchange/color_change.dart';
 import 'package:distributed_server/connmanager/connmanager.dart';
-import 'package:distributed_server/counter/counter.dart';
 
 Handler middleware(Handler handler) {
-  return handler
-      .use(counterProvider)
-      .use(connManagerProvider)
-      .use(colorChangeProvider);
+  return handler.use(connManagerProvider).use(colorChangeProvider);
 }
