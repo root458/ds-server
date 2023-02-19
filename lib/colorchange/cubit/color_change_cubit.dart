db5f5f04181eaccb1a4f418940d3acce30acdafb
Hope Kagame <kagame458@protonmail.com>
2023-02-19 16:19:37 +0300

    Enable color change propagation

--- lib/colorchange/cubit/color_change_cubit.dart
+++ lib/colorchange/cubit/color_change_cubit.dart
@@ -1,13 +1,11 @@
 import 'package:broadcast_bloc/broadcast_bloc.dart';
+import 'package:distributed_server/colorchange/color_change.dart';
 
 /// ColorChange Cubit
-class ColorChangeCubit extends BroadcastCubit<int> {
-  /// Create an instance with an initial state of 0.
-  ColorChangeCubit() : super(0);
+class ColorChangeCubit extends BroadcastCubit<CColor> {
+  /// Create an instance with an initial state of white color.
+  ColorChangeCubit() : super(CColor(color: 'white'));
 
-  /// Increment the current state.
-  void increment() => emit(state + 1);
-
-  /// Decrement the current state.
-  void decrement() => emit(state - 1);
+  /// Change the current state.
+  void changeColor(CColor color) => emit(color);
 }
