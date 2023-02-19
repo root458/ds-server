5a7abdad1e0d36b0b6ca3b90744afa1949e5a9e7
Hope Kagame <kagame458@protonmail.com>
2023-02-19 10:39:10 +0300

    Add and remove connections on route

--- routes/ws.dart
+++ routes/ws.dart
@@ -1,19 +1,24 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+import 'package:distributed_server/connmanager/connmanager.dart';
 import 'package:distributed_server/counter/counter.dart';
 
 Future<Response> onRequest(RequestContext context) async {
+  // Initialize conn manager
+  final connManagerCubit = context.read<ConnManagerCubit>();
   final handler = webSocketHandler(
     (channel, protocol) {
+      // Log address
       final address = context.request.connectionInfo.remoteAddress;
       print(address.address);
+      connManagerCubit.addConnectedClient(address.address);
       // A new client has connected to our server.
       // Subscribe the new client to receive notifications
       // whenever the cubit state changes.
-      final cubit = context.read<CounterCubit>()..subscribe(channel);
+      final counterCubit = context.read<CounterCubit>()..subscribe(channel);
 
       // Send the current count to the new client.
-      channel.sink.add('${cubit.state}');
+      channel.sink.add('${counterCubit.state}');
 
       // Listen for messages from the client.
       channel.stream.listen(
@@ -21,11 +26,11 @@ Future<Response> onRequest(RequestContext context) async {
           switch (event) {
             // Handle an increment message.
             case '__increment__':
-              cubit.increment();
+              counterCubit.increment();
               break;
             // Handle a decrement message.
             case '__decrement__':
-              cubit.decrement();
+              counterCubit.decrement();
               break;
             // Ignore any other messages.
             default:
@@ -34,7 +39,10 @@ Future<Response> onRequest(RequestContext context) async {
         },
         // The client has disconnected.
         // Unsubscribe the channel.
-        onDone: () => cubit.unsubscribe(channel),
+        onDone: () {
+          counterCubit.unsubscribe(channel);
+          connManagerCubit.removeConnectedClient(address.address);
+        },
       );
     },
   );
