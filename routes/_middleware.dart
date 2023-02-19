eb0ac46864f3f8ed2f72cb76ddf243870e5aa435
Hope Kagame <kagame458@protonmail.com>
2023-02-19 11:09:14 +0300

    Update middleware injection

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,7 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
-import 'package:distributed_server/connmanager/middleware/connmanager_provider.dart';
+import 'package:distributed_server/connmanager/connmanager.dart';
 import 'package:distributed_server/counter/counter.dart';
 
-Handler middleware(Handler handler) => handler
-  .use(counterProvider)
-  ..use(connManagerProvider);
+Handler middleware(Handler handler) {
+  return handler.use(counterProvider).use(connManagerProvider);
+}
