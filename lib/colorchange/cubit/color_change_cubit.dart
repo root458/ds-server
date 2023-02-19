1ea8900c81ad86c70587583369b26cca409580ec
Hope Kagame <kagame458@protonmail.com>
2023-02-19 17:24:30 +0300

    Enable color chnages on cubit

--- lib/colorchange/cubit/color_change_cubit.dart
+++ lib/colorchange/cubit/color_change_cubit.dart
@@ -1,11 +1,21 @@
+import 'dart:convert';
+
 import 'package:broadcast_bloc/broadcast_bloc.dart';
 import 'package:distributed_server/colorchange/color_change.dart';
 
+JsonEncoder _encoder = const JsonEncoder();
+JsonDecoder _decoder = const JsonDecoder();
+
 /// ColorChange Cubit
-class ColorChangeCubit extends BroadcastCubit<CColor> {
+class ColorChangeCubit extends BroadcastCubit<String> {
   /// Create an instance with an initial state of white color.
-  ColorChangeCubit() : super(CColor(color: 'white'));
+  ColorChangeCubit() : super(_encoder.convert(CColor(color: 'white').toJson()));
 
   /// Change the current state.
-  void changeColor(CColor color) => emit(color);
+  void changeColor(String newColor) {
+    final color =
+        CColor.fromJson(_decoder.convert(newColor) as Map<String, dynamic>);
+    print(color.toJson());
+    emit(_encoder.convert(color.toJson()));
+  }
 }
