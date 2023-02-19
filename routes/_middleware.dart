e3ccfc57f565d10fa3c3136b2b34729559645e66
Hope Kagame <kagame458@protonmail.com>
2023-02-19 13:37:52 +0300

    Provide color change logic on provider

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,7 +1,11 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:distributed_server/colorchange/color_change.dart';
 import 'package:distributed_server/connmanager/connmanager.dart';
 import 'package:distributed_server/counter/counter.dart';
 
 Handler middleware(Handler handler) {
-  return handler.use(counterProvider).use(connManagerProvider);
+  return handler
+      .use(counterProvider)
+      .use(connManagerProvider)
+      .use(colorChangeProvider);
 }
