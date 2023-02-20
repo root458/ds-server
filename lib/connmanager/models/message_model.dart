edd055a8595a31cebe659b6e7c07ce27c05bc7d8
Hope Kagame <kagame458@protonmail.com>
2023-02-20 11:44:01 +0300

    Add message model

---
+++ lib/connmanager/models/message_model.dart
@@ -0,0 +1,27 @@
+/// A constant way of sending messages to clients
+class Message {
+  /// Create a message
+  Message({
+    required this.purpose,
+    required this.data,
+    required this.clientID,
+  });
+
+  /// From Json constructor
+  factory Message.fromJson(Map<String, dynamic> message) {
+    return Message(
+      purpose: message['purpose']! as String,
+      data: message['data']! as String,
+      clientID: message['clientID']! as String,
+    );
+  }
+
+  /// What the message is about: can be _colorchange_ or _upgrade_ or _newconn_
+  final String purpose;
+
+  /// What the message is about: can be _colorchange_ or _upgrade_ or _newconn_
+  final String clientID;
+
+  /// Stringified string that contains the message
+  final String data;
+}
