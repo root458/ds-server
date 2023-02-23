4e89c6c48bdb41c26c8f540a4c3bf1676c7577c5
Hope Kagame Opiyo <57757012+root458@users.noreply.github.com>
2023-02-23 15:47:42 +0300

    Update max connections

--- lib/connmanager/middleware/connmanager_provider.dart
+++ lib/connmanager/middleware/connmanager_provider.dart
@@ -2,7 +2,7 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:distributed_server/connmanager/connmanager.dart';
 
 // Initialize with max number of connections
-final _connManager = ConnManagerCubit(5);
+final _connManager = ConnManagerCubit(20);
 
 /// Provide the connManager instance via `RequestContext`.
 final connManagerProvider = provider<ConnManagerCubit>((_) => _connManager);
